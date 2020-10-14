b44a4a75296386dde948f75baabe787f3c1c272e
SELA <RiSEblackbird@users.noreply.github.com>
2020-10-14 14:50:55 +0900

    6-13 : GoogleAuthの使用 (各値はダミー)

--- C6/board_app/pages/axios.vue
+++ C6/board_app/pages/axios.vue
@@ -1,100 +1,41 @@
-// 6-12 custom
+// 6-13
 <template>
   <section class="container">
     <h1>{{ title }}</h1>
     <p>{{ message }}</p>
-    <table>
-      <h3>Delete</h3>
-      <tr>
-        <th>Target Email</th>
-        <td><input v-model="del_email"></td>
-        <td><button @click="delData">Delete</button></td>
-      </tr>
-    </table>
-    <table>
-      <h3>Add</h3>
-      <tr>
-        <th>Email</th>
-        <td><input v-model="email"></td>
-      </tr>
-      <tr>
-        <th>Name</th>
-        <td><input v-model="username"></td>
-      </tr>
-      <tr>
-        <th>Age</th>
-        <td><input type="number" v-model="age"></td>
-      </tr>
-      <tr>
-        <th>Tel</th>
-        <td><input v-model="tel"></td>
-      </tr>
-      <tr>
-        <th></th>
-        <td><button @click="addData">Create</button></td>
-      </tr>
-    </table>
-    <hr>
-    <ul v-for="(data, key) in json_data">
-      <li><strong>{{ key }}</strong><br>{{ data }}</li>
-    </ul>
   </section>
 </template>
 
 <script>
-const axios = require('axios');
-
-let url = "https://nuxt-tyou1.firebaseio.com/person";
+import firebase from 'firebase';
 
 export default {
   data: function () {
     return {
-      title: 'Axios',
-      email: '',
-      del_email: '',
-      username: '',
-      tel: '',
-      age: 0,
-      message: 'axios sample.',
-      json_data: {}
+      title: 'Auth',
+      message: 'This is it!!!'
     };
   },
-  methods: {
-    addData: function () {
-      let add_url = url + '/' + this.email + '.json';
-      let data = {
-        'name': this.username,
-        'age': this.age,
-        'tel': this.tel
-      };
-      axios.put(add_url, data).then((re) => {
-        this.email = '';
-        this.username = '';
-        this.age = 0;
-        this.tel = '';
-        this.getData();
-      });
-    },
-    getData: function () {
-      axios.get(url + '.json').then((res) => {
-        this.message = 'get all data.';
-        this.json_data = res.data;
-      }).catch((error) => {
-        this.message = 'ERROR!';
-        this.json_data = {};
-      });
-    },
-    delData: function () {
-      let del_url = url + '/' + this.del_email + '.json';
-      axios.delete(del_url).then((re) => {
-        this.message = this.email + 'を削除しました。';
-        this.email = '';
-        this.getData();
-      });
-    }
-  },
   created: function () {
-    this.getData();
+    var firebaseConfig = {
+      apiKey: "……キー……",
+      authDomain: "プロジェクト.firebaseapp.com",
+      databaseURL: "https://プロジェクト.firebaseio.com",
+      projectId: "プロジェクト",
+      storageBucket: "プロジェクト.appspot.com",
+      messagingSenderId: "……ID……",
+      appId: "....",
+      measurementId: "...."
+    };
+    firebase.initializeApp(firebaseConfig);
+
+    var provider = new firebase.auth.GoogleAuthProvider();
+    var self = this;
+    firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+        self.message = result.user.displayName + ', ' 
+          + result.user.email;
+      });
   }
 }
 </script>
